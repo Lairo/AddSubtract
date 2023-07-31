1740ae15aeaa054410e1a9d3c7f7a4cdbcba2993
chajus1 <chajus1@googlemail.com>
2023-07-31 23:43:54 +0100

    Add project files.

---
+++ AddSubtract/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Addsubtract
+{
+    class Program {
+
+        static void Main()
+        {
+            AddSubtract a = new AddSubtract() { Value = 5 }
+                .Add(5)
+                .Subtract(3)
+                .Add(9)
+                .Subtract(12);
+            Console.WriteLine($"Result: {a.Value}");
+            Console.ReadLine();
+        }
+    }
+}
